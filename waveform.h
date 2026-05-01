32d8aef518990d9f413a89b96ae632013f6081cf
o2-touati <Ouassim2.Touati@live.uwe.ac.uk>
2026-05-01 15:56:23 +0100

    Added header guard to waveform.h to prevent multiple inclusion errors.

---
+++ waveform.h
@@ -0,0 +1,11 @@
+//
+// Created by o2-touati on 01/05/2026.
+//
+
+#ifndef COURSEWORK_AC_WAVEFORM_WAVEFORM_H
+#define COURSEWORK_AC_WAVEFORM_WAVEFORM_H
+
+#endif //COURSEWORK_AC_WAVEFORM_WAVEFORM_H
+
+#ifndef WAVEFORM_H
+#define WAVEFORM_H
