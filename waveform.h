e3540ab4ba012e1854c0bea7d85e3bc5bafee819
o2-touati <Ouassim2.Touati@live.uwe.ac.uk>
2026-05-01 16:05:44 +0100

    Defined WaveformSample struct to represent a single row of waveform data, grouping all electrical parameters (voltage, current, frequency, etc.) into a single data structure for easier management

--- waveform.h
+++ waveform.h
@@ -10,3 +10,13 @@
 #ifndef WAVEFORM_H
 #define WAVEFORM_H
 #include <stddef.h>
+typedef struct {
+    double timestamp;
+    double phase_A_voltage;
+    double phase_B_voltage;
+    double phase_C_voltage;
+    double line_current;
+    double frequency;
+    double power_factor;
+    double thd_percent;
+} WaveformSample;
