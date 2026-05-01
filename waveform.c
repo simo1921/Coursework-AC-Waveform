921b8deddd16cb03f7728d750004069e478465ec
o2-touati <Ouassim2.Touati@live.uwe.ac.uk>
2026-05-01 17:35:21 +0100

    1.Implemented compute_rms function to calculate root mean square voltage representing the effective value of the AC waveform 2.Added logic to square each waveform sample and accumulate results as part of RMS calculation 3.Finalised RMS calculation by averaging squared values and applying square root using math library

--- waveform.c
+++ waveform.c
@@ -7,3 +7,7 @@
 double compute_mean(double *data, size_t n)
 sum += *(data + i);
 return sum / n;
+double compute_rms(double *data, size_t n) // 1
+        double val = *(data + i);
+                sum += val * val; // 2
+return sqrt(sum / n); // 3
