e8adb9c3db37fb45d740e757ecd30a82fe026815
o2-touati <Ouassim2.Touati@live.uwe.ac.uk>
2026-05-02 12:36:59 +0100

    We added part 2 to io.c:
    The write_results function was added to handle outputting the analysed waveform data into a file. Instead of printing results directly in main.c, this function organises all output operations in one place, improving code structure and readability. It formats and writes key results such as RMS values, peak-to-peak, DC offset, and clipping status into a text file. This also follows a modular design approach by separating file input/output logic from data processing.

--- io.c
+++ io.c
@@ -62,3 +62,33 @@ WaveformSample* load_csv(const char *filename, size_t *count) {
 
     return data;
 }
+    void write_results(const char *filename,                              // Part 2
+                       double rmsA, double rmsB, double rmsC,
+                       double p2pA, double p2pB, double p2pC,
+                       double dcA, double dcB, double dcC,
+                       int clipA, int clipB, int clipC) {
+
+        FILE *file = fopen(filename, "w");
+        if (!file) {
+            printf("Error: Cannot write file\n");
+            return;
+        }
+
+        fprintf(file, "RMS:\nA: %.2f (%s)\nB: %.2f (%s)\nC: %.2f (%s)\n\n",
+                rmsA, check_tolerance(rmsA) ? "OK" : "Out of range",
+                rmsB, check_tolerance(rmsB) ? "OK" : "Out of range",
+                rmsC, check_tolerance(rmsC) ? "OK" : "Out of range");
+
+        fprintf(file, "Peak-to-Peak:\nA: %.2f\nB: %.2f\nC: %.2f\n\n",
+                p2pA, p2pB, p2pC);
+
+        fprintf(file, "DC Offset:\nA: %.2f\nB: %.2f\nC: %.2f\n\n",
+                dcA, dcB, dcC);
+
+        fprintf(file, "Clipping:\nA: %s\nB: %s\nC: %s\n",
+                clipA ? "Yes" : "No",
+                clipB ? "Yes" : "No",
+                clipC ? "Yes" : "No");
+
+        fclose(file);
+    }         // End of part 2
